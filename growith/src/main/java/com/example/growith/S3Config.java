13c37c12e932bc2a1f05a65c383672f927cdd9c2
freshberryy <161305732+freshberryy@users.noreply.github.com>
2024-07-12 00:49:54 +0900

    DB/aws 설정

---
+++ growith/src/main/java/com/example/growith/S3Config.java
@@ -0,0 +1,29 @@
+package com.example.growith;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3Config {
+    @Value("AKIAVRUVS2JV36PW367T")
+    private String awsAccesskey;
+
+    @Value("IUu63J0bA3Yd1bGYdn6/9hffcVQrLX00L54PRWAV")
+    private String awsSecretkey;
+
+    @Value("ap-northeast-2")
+    private String region;
+
+    @Bean
+    public AmazonS3 s3client() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsAccesskey, awsSecretkey);
+        return  AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region)).withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
+    }
+}
---
+++ growith/src/main/java/com/example/growith/S3Service.java
@@ -0,0 +1,42 @@
+package com.example.growith;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+
+    @Value("awsteam1234")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    public void uploadfile(MultipartFile multipartFile, String filename) throws IOException{
+
+        //현재 서버에 임시 저장
+        File file = new File(multipartFile.getOriginalFilename());
+
+        try(FileOutputStream fos = new FileOutputStream(file)) {
+            fos.write(multipartFile.getBytes());
+        }
+
+        //AWS 전송-UUID 적용버전, UUID로 적용된 이름을 가져와서 사용한다.
+        //String filename = System.currentTimeMillis() + "_" + multipartFile.getOriginalFilename();
+        amazonS3.putObject(new PutObjectRequest(bucketName, filename, file));
+
+        //임시 저장된 사진 삭제
+        file.delete();
+    }
+}
+
