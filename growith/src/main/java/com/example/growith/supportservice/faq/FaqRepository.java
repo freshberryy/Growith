4ef93add289d36c421b370b742610c5eb8975177
T-ferret <hs381717@gmail.com>
2024-07-16 10:34:06 +0900

    Controller 세분화, faq 기능 구현

---
+++ growith/src/main/java/com/example/growith/supportservice/faq/FaqRepository.java
@@ -0,0 +1,6 @@
+package com.example.growith.supportservice.faq;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FaqRepository extends JpaRepository<Faq, Integer> {
+}
