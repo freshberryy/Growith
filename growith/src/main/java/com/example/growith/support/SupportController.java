610814f1b6a31e8c525fcfb9e66d6371e12738bc
siwol10 <108480766+siwol10@users.noreply.github.com>
2024-07-16 10:57:57 +0900

    update NoticeController

--- growith/src/main/java/com/example/growith/support/SupportController.java
+++ growith/src/main/java/com/example/growith/support/SupportController.java
@@ -1,8 +1,10 @@
 package com.example.growith.support;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SupportController {
 	
+	@Autowired
+	private NoticeService noticeService;
+	
 	@GetMapping("/announcements")
-	public String announcements() {
+	public String announcements(Model model) {
+		
+		model.addAttribute("notices", noticeService.readList());
+		
 		return "support_announcements";
+	}
+	
+	@GetMapping("/announcements/readdetail/{id}")
+	public String readdetail(Model model, @PathVariable("id") Integer id) {
+		model.addAttribute("notice", noticeService.readdetail(id));
+		return "support_announcements_readdetail";
 	}
 
 	@GetMapping("/contact")
@@ -34,4 +48,7 @@ public class SupportController {
 	public String faq() {
 		return "support_faq";
 	}
+	
+	
+	
 }
