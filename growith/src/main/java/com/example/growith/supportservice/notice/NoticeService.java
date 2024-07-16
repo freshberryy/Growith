500769e6b15221a741854e0979604dada5d37f00
siwol10 <108480766+siwol10@users.noreply.github.com>
2024-07-16 11:22:39 +0900

    add NoticeRepository&Service

---
+++ growith/src/main/java/com/example/growith/supportservice/notice/NoticeService.java
@@ -0,0 +1,30 @@
+package com.example.growith.supportservice.notice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService {
+	@Autowired
+	private NoticeRepository noticeRepository;
+	
+	public void create(Notice notice) {
+		notice.setDate(LocalDateTime.now());
+		noticeRepository.save(notice);
+	}
+	
+	// db에 있는 board 전체 갖고 오기
+	public List<Notice> readList(){
+		return noticeRepository.findAll();
+	}
+	
+	public Notice readdetail(Integer id) {
+		Optional<Notice> o=noticeRepository.findById(id);
+		
+		return o.get(); 
+	}
+}
