2414b961d4a29cf72cd01b370611f7e158990e4a
T-ferret <132970869+T-ferret@users.noreply.github.com>
2024-07-16 10:01:17 +0900

    Delete growith/src/main/java/com/example/growith/customerservice directory
    customerservice delete

--- growith/src/main/java/com/example/growith/customerservice/inquery/InqueryTypeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.growith.customerservice.inquery;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InqueryTypeRepository extends JpaRepository<InqueryType, Integer> {
-}
