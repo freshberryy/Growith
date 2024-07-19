29fdff6ff9fc87e76b54f361f688d8dc7a2f9ba1
T-ferret <hs381717@gmail.com>
2024-07-19 18:13:08 +0900

    인메모리 형식 관리자 로그인

--- growith/src/main/java/com/example/growith/SecurityConfig.java
+++ growith/src/main/java/com/example/growith/SecurityConfig.java
@@ -1,50 +1,36 @@
 package com.example.growith;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.Arrays;
+
+@Slf4j
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
-
-    public SecurityConfig(CustomAuthenticationFailureHandler customAuthenticationFailureHandler) {
-        this.customAuthenticationFailureHandler = customAuthenticationFailureHandler;
-    }
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf ->
-                        csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                )
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers(new AntPathRequestMatcher("/admin/**")).authenticated()
-                                .anyRequest().permitAll()
-                )
-                .formLogin(formLogin ->
-                        formLogin.loginPage("/admin/login")
-                                .defaultSuccessUrl("/")
-                                .usernameParameter("email") //로그인 폼의 username 파라미터를 email로 변경
-                                .failureHandler(customAuthenticationFailureHandler) // 로그인 실패 로그 핸들러
-                                .permitAll()
-                )
-                .logout(logout ->
-                        logout.permitAll()
-                );
-        return http.build();
-    }
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -52,7 +38,80 @@ public class SecurityConfig {
     }
 
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails manager = User.withUsername("test@email")
+                .password(passwordEncoder().encode("password"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(manager);
+    }
+
+//    @Bean
+//    AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+//        AuthenticationManagerBuilder authenticationManagerBuilder =
+//                http.getSharedObject(AuthenticationManagerBuilder.class);
+//        authenticationManagerBuilder.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
+//        return authenticationManagerBuilder.build();
+//    }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager() {
+//        DaoAuthenticationProvider inMemoryProvider = new DaoAuthenticationProvider();
+//        inMemoryProvider.setUserDetailsService(inMemoryUserDetailsManager());
+//        inMemoryProvider.setPasswordEncoder(passwordEncoder());
+//
+//        DaoAuthenticationProvider customProvider = new DaoAuthenticationProvider();
+//        customProvider.setUserDetailsService(customUserDetailService);
+//        customProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return new ProviderManager(Arrays.asList(inMemoryProvider, customProvider));
+//    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(inMemoryUserDetailsManager()).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> {
+                            log.debug("Configuring CSRF protection");
+                            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                        }
+                )
+                .authorizeHttpRequests(authorizeRequests -> {
+                            log.debug("Configuring authorizeRequests");
+                            authorizeRequests
+                                    .requestMatchers("/admin/login").permitAll()
+                                    .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
+                                    .anyRequest().permitAll();
+                        }
+                )
+                .formLogin(formLogin -> {
+                            log.debug("Configuring form Login");
+                            formLogin.loginPage("/admin/login")
+                                    .defaultSuccessUrl("/admin/notice", true)
+                                    .usernameParameter("email") //로그인 폼의 username 파라미터를 email로 변경
+                                    .failureHandler(customAuthenticationFailureHandler) // 로그인 실패 로그 핸들러
+                                    .permitAll();
+                        }
+                )
+                .logout(logout -> {
+                            log.debug("Configuring Logout");
+                            logout.logoutUrl("/admin/logout")
+                                    .logoutSuccessUrl("/")
+                                    .permitAll();
+                        }
+                )
+                .exceptionHandling(exceptionHandling -> {
+                    log.debug("Configuring exceptionHandling");
+                    exceptionHandling.accessDeniedHandler(customAccessDeniedHandler);
+                });
+        return http.build();
     }
 }
--- growith/src/main/java/com/example/growith/member/MemberService.java
+++ growith/src/main/java/com/example/growith/member/MemberService.java
@@ -1,17 +1,63 @@
 package com.example.growith.member;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    @Lazy
+    private final PasswordEncoder passwordEncoder;
+
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        Member member = memberRepository.findByEmail(username).orElse(null);
+//        if (member != null) {
+//            List<GrantedAuthority> authorities = new ArrayList<>();
+//            if ("ROLE_ADMIN".equals(member.getRole())) {
+//                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+//            }
+//            return new User(member.getEmail(), member.getPassword(), authorities);
+//        } else {
+//            throw new UsernameNotFoundException("유저를 찾을 수 없습니다: " + username);
+//        }
+//
+//    }
+
+//    public void create(Member member) {
+//        member.setRole("ROLE_USER");
+////        member.setDate(LocalDateTime.now());
+//        member.setPassword(passwordEncoder.encode(member.getPassword()));
+//        memberRepository.save(member);
+//    }
+
+    public void create(String username, String password) {
+        Member member = new Member();
+        member.setEmail(username);
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        member.setRole("ROLE_ADMIN");
+        memberRepository.save(member);
+    }
+
+//    public Member findByEmail(String email) {
+//        Member member = memberRepository.findByEmail(email).orElse(null);
+//        return member;
+//    }
 
     public Member authen() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
