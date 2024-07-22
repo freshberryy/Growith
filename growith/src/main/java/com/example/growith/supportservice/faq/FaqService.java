2b5aa7ff15fc805eb324a4852e295a9ab4beae58
T-ferret <hs381717@gmail.com>
2024-07-22 15:13:05 +0900

    service 분리

--- growith/src/main/java/com/example/growith/supportservice/faq/FaqService.java
+++ growith/src/main/java/com/example/growith/supportservice/faq/FaqService.java
@@ -1,10 +1,7 @@
 package com.example.growith.supportservice.faq;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,27 +9,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class FaqService {
-	
-	@Autowired
     private final FaqRepository faqRepository;
 
-    public void create(Faq faq) {
-        faq.setDate(LocalDateTime.now());
-        faqRepository.save(faq);
+    // db에 있는 board 전체 갖고 오기
+    public List<Faq> readList() {
+        return faqRepository.findAll();
     }
-
-    public void delete(Integer id) {
-		faqRepository.deleteById(id);
-	}
-	
-	// db에 있는 board 전체 갖고 오기
-	public List<Faq> readList(){
-		return faqRepository.findAll();
-	}
-	
-	public Faq readdetail(Integer id) {
-		Optional<Faq> o = faqRepository.findById(id);
-		
-		return o.get(); 
-	}
 }
