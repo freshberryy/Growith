17f1a6400075bf198c6962d4eb33828d1fed8644
T-ferret <hs381717@gmail.com>
2024-07-22 15:39:21 +0900

    문의사항 유형 서비스/faq 페이징 추가

--- growith/src/main/java/com/example/growith/supportservice/faq/FaqService.java
+++ growith/src/main/java/com/example/growith/supportservice/faq/FaqService.java
@@ -2,6 +2,9 @@ package com.example.growith.supportservice.faq;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -12,7 +15,12 @@ public class FaqService {
     private final FaqRepository faqRepository;
 
     // db에 있는 board 전체 갖고 오기
-    public List<Faq> readList() {
-        return faqRepository.findAll();
+//    public List<Faq> readList() {
+//        return faqRepository.findAll();
+//    }
+
+    public Page<Faq> getFaqs(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return faqRepository.findAll(pageable);
     }
 }
